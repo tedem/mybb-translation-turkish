bd6d2bc9809dfd7456625620f608bf0b20b942fe
Medet Erdal <hello@tedem.dev>
2025-12-26 08:48:47 +0300

    Translate customhelpdocs.lang.php

--- inc/languages/turkish/customhelpdocs.lang.php
+++ inc/languages/turkish/customhelpdocs.lang.php
@@ -9,7 +9,7 @@
  * Custom Help Document Translation Format
  *
  * // Help Document {hid}
- * $l['d{hid}_name'] = "Document name";
- * $l['d{hid}_desc'] = "Document description";
- * $l['d{hid}_document'] = "Document text";
+ * $l['d{hid}_name'] = "Doküman adı";
+ * $l['d{hid}_desc'] = "Doküman açıklaması";
+ * $l['d{hid}_document'] = "Doküman metni";
  */
