902ea3ae3cd2016281c04a9b745a8d8743be896c
Medet Erdal <hello@tedem.dev>
2025-12-26 08:47:09 +0300

    Translate contact.lang.php

--- inc/languages/turkish/contact.lang.php
+++ inc/languages/turkish/contact.lang.php
@@ -5,21 +5,21 @@
  *
  */
 
-$l['contact'] = 'Contact Us';
-$l['contact_no_message'] = 'You have not provided a message to send.';
-$l['contact_no_subject'] = 'You are required to enter a subject.';
-$l['contact_no_email'] = 'You must enter a valid email address.';
-$l['contact_success_message'] = 'Your message has been successfully sent to the Administrator.';
-$l['contact_subject'] = 'Subject';
-$l['contact_subject_desc'] = 'Brief the topic of your message.';
-$l['contact_message'] = 'Message';
-$l['contact_message_desc'] = 'Describe your concern in this area in detail.';
-$l['contact_email'] = 'Email';
-$l['contact_email_desc'] = 'Enter your email so we can contact you back.';
-$l['contact_send'] = 'Send';
-$l['subject_too_long'] = 'The subject is too long. Please enter a subject shorter than {1} characters (currently {2}).';
-$l['message_too_short'] = 'The message is too short. Please enter a message longer than {1} characters (currently {2}).';
-$l['message_too_long'] = 'The message is too long. Please enter a message shorter than {1} characters (currently {2}).';
+$l['contact'] = 'Bize Ulaşın';
+$l['contact_no_message'] = 'Mesaj göndermek için bir mesaj sağlamadınız.';
+$l['contact_no_subject'] = 'Bir konu girmeniz gerekmektedir.';
+$l['contact_no_email'] = 'Geçerli bir e-posta adresi girmeniz gerekmektedir.';
+$l['contact_success_message'] = 'Mesajınız başarıyla Yöneticiye gönderildi.';
+$l['contact_subject'] = 'Konu';
+$l['contact_subject_desc'] = 'Mesajınızın konusunu kısaca belirtin.';
+$l['contact_message'] = 'Mesaj';
+$l['contact_message_desc'] = 'Endişenizi bu alanda ayrıntılı olarak açıklayın.';
+$l['contact_email'] = 'E-posta';
+$l['contact_email_desc'] = 'Size geri dönebilmemiz için e-posta adresinizi girin.';
+$l['contact_send'] = 'Gönder';
+$l['subject_too_long'] = 'Konu çok uzun. Lütfen {1} karakterden daha kısa bir konu girin (şu anda {2}).';
+$l['message_too_short'] = 'Mesaj çok kısa. Lütfen {1} karakterden daha uzun bir mesaj girin (şu anda {2}).';
+$l['message_too_long'] = 'Mesaj çok uzun. Lütfen {1} karakterden daha kısa bir mesaj girin (şu anda {2}).';
 
-$l['error_stop_forum_spam_spammer'] = 'Sorry, {1} matches that of a known spammer and therefore your contact submission has been blocked.';
-$l['error_stop_forum_spam_fetching'] = 'Sorry, something went wrong verifying your message against a spammer database. Most likely the database couldn\'t be accessed. Please try again later.';
+$l['error_stop_forum_spam_spammer'] = 'Üzgünüz, {1} bilinen bir spamcı ile eşleşiyor ve bu nedenle iletişim gönderiminiz engellendi.';
+$l['error_stop_forum_spam_fetching'] = 'Üzgünüz, mesajınızın spamcı veritabanına karşı doğrulanmasında bir sorun oluştu. Muhtemelen veritabanına erişilemedi. Lütfen daha sonra tekrar deneyin.';
